d2f1c3b900fcfd96ed366767fb8c34b5e5f4a52c
Thomas Rolfe <thomasrolfe@yahoo.co.uk>
2019-12-29 10:51:54 +0000

    nesting components. v-for to create tasks list

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
 
+Vue.component('task-list', {
+    template: `
+    <div>
+        <task v-for="task in tasks">
+            {{ task.task }}
+        </task>
+    </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                {task: 'Go to the store', complete: true},
+                {task: 'Go to the bank', complete: false},
+                {task: 'Go to the work', complete: true},
+                {task: 'Go to the park', complete: false}
+
+            ]
+        }
+    }
+});
+
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        tasks: ['Go to the store', 'Go to the bank', 'finish tutorial']
+    }
 });
